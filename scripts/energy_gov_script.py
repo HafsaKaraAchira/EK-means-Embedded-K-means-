e6b581d587c91972471b5c607a03e4c92837d738
HafsaKaraAchira <59230432+HafsaKaraAchira@users.noreply.github.com>
2023-01-10 18:12:34 +0100

    LOG conservative 500M_D 235_RAM, CM_275M, SEP 0,3

--- scripts/energy_gov_script.py
+++ scripts/energy_gov_script.py
@@ -1,5 +1,6 @@
 
 import os
+import time
 
 from pyJoules.device.rapl_device import RaplCoreDomain , RaplDramDomain
 from pyJoules.handler.csv_handler import CSVHandler
@@ -12,29 +13,33 @@ from pyJoules.energy_meter import EnergyContext
 	
 csv_handler = CSVHandler('./logs/log_energy.csv')
 
-
+file = "generator/CM5,8M_1000MO_SEP0,3/points.csv"
+N = 5800000
+k = 10
+dim = 10
 
 #@measure_energy(handler=csv_handler, domains=[RaplCoreDomain(0),RaplDramDomain(0)])
-def program_call():
-    os.system("./program")
+def program_call(M):
+    os.system("./program "+file+" "+str(k)+" "+str(N)+" "+str(int(N/M))+" "+str(dim))
 
 affinity_mask = {1}
 pid = 0
 os.sched_setaffinity(0, affinity_mask)
 
-governors = ["ondemand","conservative","schedutil","performance","powersave"];
-available_memory = [100,200,300,400,500,600,700,800,900,1000]
+governors = ["ondemand"]  #["performance","conservative","schedutil","performance","powersave"];
+memory_constraint = [1] #0.8,1,2,4,5,8,10]
 
-for M in reversed(available_memory) :
+for M in memory_constraint :
     # $1 is the governor name
-    os.system("echo $(( "+str(M)+" * 1024 * 1024)) > /sys/fs/cgroup/memory/prog_cgroup/memory.limit_in_bytes")
-    print(M)
+    os.system("echo $(( "+str(int(1000/M))+" * 1024 * 1024)) > /sys/fs/cgroup/memory/kmeans/memory.limit_in_bytes")
+    print(str(M)+"  "+str(int(1000/M))+"    "+str(int(N/M)))
     for governor in governors :
-        os.system("./scripts/prog_script_cache")
         os.system("echo "+governor+" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
+        os.system("./scripts/prog_script_cache")
+        time.sleep(30)
         with EnergyContext(handler=csv_handler, domains=[RaplCoreDomain(0),RaplDramDomain(0)], start_tag=governor+","+str(M)) as ctx:
             # call the target program
-            program_call()
+            program_call(M)
 
 
 csv_handler.save_data()
