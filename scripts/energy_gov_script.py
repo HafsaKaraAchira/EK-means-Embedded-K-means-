fc2cccd1dd4cb0b4455c1ce931288058959b09a2
HafsaKaraAchira <59230432+HafsaKaraAchira@users.noreply.github.com>
2023-01-12 17:39:00 +0100

    log conservative M=1078

--- scripts/energy_gov_script.py
+++ scripts/energy_gov_script.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import math
 
 from pyJoules.device.rapl_device import RaplCoreDomain , RaplDramDomain
 from pyJoules.handler.csv_handler import CSVHandler
@@ -13,8 +14,8 @@ from pyJoules.energy_meter import EnergyContext
 	
 csv_handler = CSVHandler('./logs/log_energy.csv')
 
-file = "generator/CM5,8M_1000MO_SEP0,3/points.csv"
-N = 5800000
+file = "generator/CM13,4M_2400MO_SEP0,2/points.csv"
+N = 13421800
 k = 10
 dim = 10
 
@@ -26,22 +27,40 @@ affinity_mask = {1}
 pid = 0
 os.sched_setaffinity(0, affinity_mask)
 
-governors = ["ondemand"]  #["performance","conservative","schedutil","performance","powersave"];
-memory_constraint = [1] #0.8,1,2,4,5,8,10]
+governors = ["ondemand","performance","conservative","schedutil"] #,"powersave"]
+memory_constraint = [1,2,4,5,8,10]
 
-for M in memory_constraint :
+def prog_memory_est(N,dim,k,MC) :
+    double_size = 8
+    int_size = 4
+    M = int(N/MC)
+    chunk_size = M * dim * double_size
+    dist_mat_size = M * k * double_size
+    kmeans_assign_size = 2 * M *int_size
+    # chunk_assign_size = M * int_size
+    # dataset_assign_size = N * int_size
+    
+    size = chunk_size+dist_mat_size+kmeans_assign_size #+chunk_assign_size
+    size = math.ceil(( (size) / (1024**2)) + 1.5 ) +1
+    
+    return size
+    
+    
+
+for MC in reversed(memory_constraint) :
     # $1 is the governor name
-    os.system("echo $(( "+str(int(1000/M))+" * 1024 * 1024)) > /sys/fs/cgroup/memory/kmeans/memory.limit_in_bytes")
-    print(str(M)+"  "+str(int(1000/M))+"    "+str(int(N/M)))
-    for governor in governors :
-        os.system("echo "+governor+" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
-        os.system("./scripts/prog_script_cache")
-        time.sleep(30)
-        with EnergyContext(handler=csv_handler, domains=[RaplCoreDomain(0),RaplDramDomain(0)], start_tag=governor+","+str(M)) as ctx:
-            # call the target program
-            program_call(M)
+    S=prog_memory_est(N,dim,k,MC)
+    # os.system("echo $(( "+str(int(S))+" * 1024 * 1024)) > /sys/fs/cgroup/memory/kmeans/memory.limit_in_bytes")
+    print(str(MC)+"  "+str(int(1024/MC))+"    "+str(S))
+    # for governor in governors :
+    #     os.system("echo "+governor+" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
+    #     os.system("./scripts/prog_script_cache")
+    #     time.sleep(30)
+    #     with EnergyContext(handler=csv_handler, domains=[RaplCoreDomain(0),RaplDramDomain(0)], start_tag=str(MC)+","+governor) as ctx:
+    #         # call the target program
+    #         program_call(MC)
 
 
-csv_handler.save_data()
+#csv_handler.save_data()
 
 #################################################################
